7b499b2af17532ae22fc0b641401c6d853d8b91f
Flavio Marques <flavio.marques@live.com>
2025-11-06 16:48:51 -0300

    #TesteGit

--- EtiquetaFORNew/Program.cs
+++ EtiquetaFORNew/Program.cs
@@ -17,6 +17,7 @@ namespace EtiquetaFORNew
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+            //testeGit
         }
     }
 }
